448b39d1400a1edad8d82de548ec41d6a45e4fb7
pyosunyoung <tjsdudtbvj12@naver.com>
2024-07-24 02:20:55 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
 import './App.css';
 import Box from './component/Box';
 
 function App() {
-  let count = useSelector(state=>state.count) 
-  // selector는 함수를 매개변수로 받음, state는 store객체에 있는 값중 count값을 가져온것
+  let count = useSelector(state => state.count);
   let id = useSelector(state => state.id);
   let password = useSelector(state => state.password);
   let dispatch = useDispatch();
 
   const increase = () => {
-    dispatch({type:"INCREMENT", payload:{num:5}}) // 
-    // dispatch를 통해 action을 던져줌 action은 단지 {}객체이다. {}안에는 type과 payload가 들어감
-    // type에는 action의 이름이 들어감
-    // payload에는 함수의 매개변수 같은 느낌 내가 원하는 정보를 보내줄 수 있음, 5씩 증가시키겠다
-    //reducer는 dispatch가 던진 액션을 받아올 수 있다.
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
   }
+
+  const decrease = () => {
+    dispatch({ type: "DECREASE", payload: { num: 5 } });
+  }
+
   const login = () => {
-    dispatch({type:"LOGIN", payload:{id:"pyo", password:"123"}})
+    dispatch({ type: "LOGIN", payload: { id: "pyo", password: "123" } });
   }
+
+  const waterHeight = Math.min(Math.max(count, -100), 100); // -100 ~ 100 범위로 제한
+  const waterStyle = {
+    height: `${Math.abs(waterHeight)}%`
+  };
+  const waterClass = waterHeight >= 0 ? 'water positive' : 'water negative';
+
   return (
-    <div>
-      <h1>{id}, {password}</h1>
-      
-      <h1>{count}</h1> 
-      <button onClick={increase}>증가버튼!</button>
-      <button onClick={login}>Login</button>
+    <div className="container">
+      <div className="header">물병에 물채우기</div>
+      <div className="login-container">
+        <button onClick={login}>Login</button>
+        <h1>{id} {password}</h1>
+      </div>
+      <h1>{count}</h1>
+      <div className="bottle-container">
+        <div className={`${waterClass}`} style={waterStyle}></div>
+      </div>
+      <div className="buttons">
+        <button onClick={increase} className='increase'>물 채우기 +</button>
+        <button onClick={decrease} className='decrease'>물 빼내기 -</button>
+      </div>
       <Box />
     </div>
-  );// count값은 
+  );
 }
 
 export default App;
-
-
-// Component ->(useDispatch) Action ->Reducer ->Store
-//       ↓                                        ↓  
-//       ------------------useSelector-------------
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,33 +1,25 @@
-
-
 let initialState = {
-  count : 0,
-  id : "",
-  password : "",
+  count: 0,
+  id: "",
+  password: "",
 }
 
-function reducer(state = initialState, action) { // reducer에 매개변수 두개가 들감
-  console.log("action을 멀까?",action); // {type: 'INCREMENT'} dispatch의 action type이 출력됨
-  if(action.type === "INCREMENT") {
-    return {...state, count:state.count + action.payload.num}; // 이 return값을 통해서 store값을 바꿈
-    // action.payload.num // dispatch에서 action type과 payload의 num값을 받음
-  } else if(action.type === "LOGIN") {
-    return {...state, id:action.payload.id, password : action.payload.password}
-  } // ...state count를 유지하고 id와 password값을 바꿔줘서 store에 던져주겠다.
-  
-  return {...state};
-  
-  // 그래서 reducer는 항상 return값을 가져야 함
-// ...state는 initalsatate안에는 여러개 state값이 존재할텐데 그 여러개 state값은 유지를 하되
-// count값만 바꾼다는 뜻임, 새로운 객체를 전달받아야 store가 본인이 바뀐걸 인지한다
-// 주고값이 같은 객체만 받으면 바뀐걸 인식 못한다.
-// 그래서  ...state는 기본적으로 치고 들어가야한다.
-// store는 return이 무조건 있어야해서 reducer에서는 무조건 기본리턴값이 필요함
-}// 행동 지침이 들어감
-//reducer는 if문, switch문 두개가 아무거나 써도됨 회사마다 다름
-// switch(action.type) {
-//   case "INCREMENT" : 
-//     return { ...state, count : count:state.count + 1};
-//   default:
-//     return {...state}; 
+function reducer(state = initialState, action) {
+  console.log("action을 멀까?", action);
+
+  if (action.type === "INCREMENT") {
+    let newCount = state.count + action.payload.num;
+    if (newCount > 100) newCount = 100; // 최대값 100으로 제한
+    return { ...state, count: newCount };
+  } else if (action.type === "LOGIN") {
+    return { ...state, id: action.payload.id, password: action.payload.password };
+  } else if (action.type === "DECREASE") {
+    let newCount = state.count - action.payload.num;
+    if (newCount < -100) newCount = -100; // 최소값 -100으로 제한
+    return { ...state, count: newCount };
+  }
+
+  return { ...state };
+}
+
 export default reducer;
